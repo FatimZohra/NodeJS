97640f59760baadad181d9a39e9b62c356ced28c
hibatollah <hibatollah93@gmail.com>
2018-01-20 20:08:10 +0000

    Add files via upload

---
+++ Exercice1.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+
+var fs = require("fs"),
+    path = require("path");
+ //afficher les fichiers du dossier parent 
+var p = "./"
+fs.readdir(p, function (err, files) {
+    if (err) {
+        throw err;
+    }
+
+    files.map(function (file) {
+        return path.join(p, file);
+    }).filter(function (file) {
+        return fs.statSync(file).isFile();
+    }).forEach(function (file) {
+        console.log("%s (%s)", file, path.extname(file));
+    });
+});
+
+ //creer un dossier si non existant
+ var dir = './monDossier';
+if (!fs.existsSync(dir)){
+    fs.mkdirSync(dir);
+}
